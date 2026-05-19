48d63a0f7c2f2101953021300f76f10464991cae
Hostilejoe2 <220639277+HostileJoe2@users.noreply.github.com>
2026-05-19 13:49:02 -0500

    Got the calculator working a little better, still working out bugs.

--- scripts.js
+++ scripts.js
@@ -3,8 +3,14 @@ const display = document.querySelector(".displayText")
 let num1
 let num2
 let operator
-let input
 let result
+
+const operations = {
+    "+": (a, b) => Number(a) + Number(b),
+    "-": (a, b) => Number(a) - Number(b),
+    "×": (a, b) => Number(a) * Number(b),
+    "÷": (a, b) => Number(a) / Number(b),
+}
 
 buttons.forEach(button => {
 
@@ -17,47 +23,66 @@ buttons.forEach(button => {
             case "-":
             case "×":
             case "÷":
+                if (num2 != undefined && result == undefined) {
+                    display.textContent = operations[operator](num1, num2);
+                }
+                if (num1 != undefined && num2 == undefined) {
+                    operator = value;
+                    let newNumber = display.textContent.slice(0,-1);
+                    display.textContent = newNumber;
+                }
                 operator = value;
-                num1 = display.textContent;
-                display.textContent = "";
+                num1 = Number(display.textContent);
+                display.textContent += value;
                 break;
 
             case "%":
-                display.textContent = Number(display.textContent) / 100
+                display.textContent = Number(display.textContent) / 100;
                 break;
 
             case "=":
-                num2 = display.textContent;
-                display.textContent = operations[operator](num1, num2);
+                if (result != undefined && result != NaN) {
+                    num2 = result
+                    result = operations[operator](num1, num2);
+                    display.textContent = result; 
+                } else {
+                const parts = display.textContent.split(operator);
+                num1 = Number(parts[0]);
+                num2 = Number(parts[1]);
+                result = operations[operator](num1, num2);
+                display.textContent = result;
+                }
                 break;
 
             case "AC":
                 num1 = null;
                 num2 = null;
                 operator = null;
-                display.textContent = ""
+                result = null;
+                display.textContent = "0"
                 break;
 
             case "⌫":
-                let newNumber = display.textContent.slice(0,-1)
-                display.textContent = newNumber
+                let newNumber = display.textContent.slice(0,-1);
+                display.textContent = newNumber;
                 break;
 
             case "±":
-                display.textContent = Number(display.textContent) * Number(-1)
+                display.textContent = Number(display.textContent) * Number(-1);
                 break;
             
             default:
-                display.textContent += value;
+                if (num1 != undefined) {
+                    num2 = Number(e.target.textContent)
+                };
+                if (display.textContent === "0" || display.textContent == result) {
+                    display.textContent = value  // replace the lone zero
+                } else {
+                    display.textContent += value // otherwise append
+                };
         }
-        console.log(num1, num2, operator)
-        
-    })
-})     
 
-const operations = {
-    "+": (a, b) => Number(a) + Number(b),
-    "-": (a, b) => Number(a) - Number(b),
-    "×": (a, b) => Number(a) * Number(b),
-    "÷": (a, b) => Number(a) / Number(b),
-}
+        console.table(num1, num2, operator, result);
+        console.log(e.target.textContent)
+    })
+})
